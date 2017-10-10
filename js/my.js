0fb63064cb9e5d5cf8386e8bd65ba301091b3af2
gopal10 <gopalpanchal10@gmail.com>
2017-10-10 23:14:39 +0530

    Add files via upload
    css files for admin and all

---
+++ js/my.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+   $("#img1").hide();
+$("#thumbnail1").click(function(){
+$("#img1").toggle(1000);    
+ $("#img1").click(function(){
+    $(this).hide(); 
+ });  
+});
+    
+     $("#img2").hide();
+$("#thumbnail2").click(function(){
+$("#img2").toggle(1000);    
+   $("#img2").click(function(){
+    $(this).hide(); 
+ });  
+});
+    
+     $("#img3").hide();
+$("#thumbnail3").click(function(){
+$("#img3").toggle(1000); 
+    $("#img3").click(function(){
+    $(this).hide(); 
+ });  
+   
+});
+    
+     $("#img4").hide();
+$("#thumbnail4").click(function(){
+$("#img4").toggle(1000); 
+    $("#img4").click(function(){
+    $(this).hide(); 
+ });  
+   
+});
+     $("#img5").hide();
+$("#thumbnail5").click(function(){
+$("#img5").toggle(1000);    
+   $("#img5").click(function(){
+    $(this).hide(); 
+ });  
+});
+    
+     $("#img6").hide();
+$("#thumbnail6").click(function(){
+$("#img6").toggle(1000);    
+   $("#img6").click(function(){
+    $(this).hide(); 
+ });  
+});
+    
+     $("#img7").hide();
+$("#thumbnail7").click(function(){
+$("#img7").toggle(1000);    
+   $("#img7").click(function(){
+    $(this).hide(); 
+ });  
+});
+    
+     $("#img8").hide();
+$("#thumbnail8").click(function(){
+$("#img8").toggle(1000);    
+   $("#img8").click(function(){
+    $(this).hide(); 
+ });  
+});
+    
+    $("img").click(function(){
+       $(this).toggleClass("animate"); 
+    });
+    
+});
